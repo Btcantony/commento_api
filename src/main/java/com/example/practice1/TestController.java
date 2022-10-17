aae3561ac109284940013e7d79764071bd233bc7
Antony <vkdrkd12@gmail.com>
2022-10-18 01:38:32 +0900

    주석 추가

--- src/main/java/com/example/practice1/TestController.java
+++ src/main/java/com/example/practice1/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    //hello world 띄우기
     @GetMapping("/v1/test")
     public String helloWorld() {
         return "hello world";
